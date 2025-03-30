4f0482dcadd4732973e77c6219d8670d513bfd34
dlopber091 <163046634+dlopber091@users.noreply.github.com>
2025-03-30 21:04:36 +0200

    Revert "Configuración del script de inicio"
    This reverts commit d4a7c2a3093a90ecc3db7b27ba7fd5329701eb85.

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Realiza git pull para obtener los cambios más recientes
-cd /opt/odoo
-git pull origin main  # Asegúrate de que 'main' es la rama correcta
-
-# Ahora inicia Odoo
-exec /entrypoint.sh odoo
