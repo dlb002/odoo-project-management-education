5cbfeb8522c56d53c900cf308748ab3d807020cf
dlopber091 <163046634+dlopber091@users.noreply.github.com>
2025-05-12 18:57:39 +0200

    modify : proyect structure
    This reverts commit d4a7c2a3093a90ecc3db7b27ba7fd5329701eb85.

---
+++ addons/custom-ies-delgado-hernandez/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Custom IES Delgado Hernandez',
+    'version': '1.0',
+    'summary': 'Personalizaciones para la gestión de proyectos',
+    'description': 'Este módulo extiende la funcionalidad de Odoo para la gestión de proyectos, añadiendo campos personalizados.',
+    'author': 'Daniel López Bermúedz',
+    'category': 'Project',
+    'depends': ['project'],
+    'data': [
+        # Archivos XML para vistas, acciones, etc.
+        # uc7f-fzmy-ictr
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
