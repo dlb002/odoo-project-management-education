96c0c48153b4e5af3112d6e1cc23dc38ab61a65c
dlopber091 <163046634+dlopber091@users.noreply.github.com>
2025-06-10 21:15:18 +0200

    fix : rute

--- backup_to_drive.sh
+++ backup_to_drive.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-BACKUP_DIR="./odoo_backups"
+BACKUP_DIR="/var/lib/odoo/.local/share/Odoo/backups/ies_delgado_hernandez"
 LOG_FILE="$HOME/rclone_backup.log"
 REMOTE_NAME="googledrive"
-REMOTE_DIR="backup_odoo"
+REMOTE_DIR="backup_odoo/ies_delgado_hernandez"
 
-echo "Sincronizando backups con Google Drive..."
+echo "Sincronizando backups con Google Drive desde $BACKUP_DIR ..."
 
-rclone sync "$BACKUP_DIR" "$REMOTE_NAME:$REMOTE_DIR" --log-level INFO --log-file "$LOG_FILE"
+rclone sync "$BACKUP_DIR" "$REMOTE_NAME:$REMOTE_DIR" --log-level DEBUG --log-file "$LOG_FILE"
 
 if [ $? -eq 0 ]; then
   echo "Sincronización completada correctamente."
