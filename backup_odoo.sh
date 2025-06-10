b9f8769daf20d3f17b8395b016016baa71756548
dlopber091 <163046634+dlopber091@users.noreply.github.com>
2025-06-10 16:53:43 +0200

    update : correct name of db

--- backup_odoo.sh
+++ backup_odoo.sh
@@ -4,7 +4,7 @@
 mkdir -p ./backups
 
 # Nombre de la BDD a respaldar.
-DB_NAME=postgres
+DB_NAME=ies_delgado_hernandez
 
 # Credenciales y configuración.
 DB_USER=odoo
@@ -12,13 +12,14 @@ DB_CONTAINER=odoo-project-management-education-db-1
 BACKUP_DIR=./backups
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 FILE_NAME="${DB_NAME}_${DATE}.sql"
+FILE_PATH="$BACKUP_DIR/$FILE_NAME"
 
 # Comando pg_dump ejecutado dentro del contenedor.
-docker exec -t $DB_CONTAINER pg_dump -U $DB_USER $DB_NAME > $BACKUP_DIR/$FILE_NAME
+docker exec -t $DB_CONTAINER pg_dump -U $DB_USER $DB_NAME > $FILE_PATH
 
 if [ -f "$FILE_PATH" ]; then
     SIZE=$(ls -lh "$FILE_PATH" | awk '{print $5}')
-    echo "Backup guardado en $BACKUP_DIR/$FILE_NAME (tamaño: $SIZE)"
+    echo "Backup guardado en $FILE_PATH (tamaño: $SIZE)"
 else
     echo "Error: no se pudo crear el backup."
 fi
